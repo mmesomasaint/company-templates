b17ff0a79d4d5e0150608f083c3c3a4b8aff06ee
Mmesoma Saint <mmesomasaint@gmail.com>
2023-01-28 07:13:30 +0100

    Fix: Make page responsive to small mobile screen

--- pages/templates/local-laundry/index.js
+++ pages/templates/local-laundry/index.js
@@ -28,12 +28,12 @@ export default function Home() {
               width={150}
               height={170}
               alt='logo'
-              className=''
+              className='w-[100px] sm:[150px]'
             />
           </div>
           <div className='flex justify-between gap-6 lg:gap-12 items-center'>
             <div className='lg:hidden'>
-              <GiHamburgerMenu className='text-3xl text-[#76A6A6]' />
+              <GiHamburgerMenu className='text-2xl sm:text-3xl text-[#76A6A6]' />
             </div>
             <nav className='hidden lg:flex justify-evenly items-start gap-6'>
               <span className='text-base font-medium leading-none text-[#D97941] scale-105 hover:scale-105 hover:text-[#D97941] w-fit'>
@@ -58,7 +58,7 @@ export default function Home() {
                 <Link href='/templates/local-laundry/my-amount'>My Amount</Link>
               </span>
             </nav>
-            <button className='py-3 px-5 bg-[#76A6A6] rounded-md'>
+            <button className='p-0 w-7 h-7 flex justify-center items-center sm:block sm:w-auto sm:h-auto sm:py-3 sm:px-5 bg-[#76A6A6] rounded-md'>
               <span className='flex justify-start items-center gap-4'>
                 <BsTelephone className='text-base text-white' />
                 <span className='hidden text-base font-medium leading-none text-white lg:flex justify-center items-center gap-1'>
@@ -76,10 +76,10 @@ export default function Home() {
         <div className='max-w-[90rem] mx-auto'>
           <div className="h-fit md:h-[55rem] w-full bg-[url('/imgs/local-laundry/showcase.jpg')] bg-no-repeat bg-cover bg-top">
             <div className='w-full h-full'>
-              <div className='w-full mx-auto bg-transparent h-full pt-32 pb-40 md:pb-0 px-8 xl:px-16  grid grid-cols-1 md:grid-cols-2 place-items-stretch gap-6 lg:gap-0'>
+              <div className='w-full mx-auto bg-transparent h-full pt-24 sm:pt-32 pb-40 md:pb-0 px-8 xl:px-16 grid grid-cols-1 md:grid-cols-2 place-items-stretch gap-6 lg:gap-0'>
                 <div className='flex flex-col justify-start items-center'>
                   <div className='w-fit mx-auto'>
-                    <h1 className='w-full xl:w-[70%] capitalize text-6xl md:text-5xl lg:text-6xl font-[900] mb-7 mt-1 text-center md:text-left text-black leading-tight'>
+                    <h1 className='w-full xl:w-[70%] capitalize text-4xl sm:text-6xl md:text-5xl lg:text-6xl font-[900] mb-7 mt-1 text-center md:text-left text-black leading-tight'>
                       New York&apos;s First Choice in Dry Cleaning
                     </h1>
                   </div>
@@ -110,70 +110,70 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className='h-[10rem] md:h-[15rem] relative'>
+          <div className='h-[15rem] sm:h-[10rem] md:h-[15rem] relative'>
             <div className='absolute h-fit -top-[7rem] md:-top-[15rem] lg:-top-[12rem] left-0 right-0 mx-8 lg:mx-16 bg-white'>
-              <div className='w-full h-full grid grid-cols-4 grid-rows-2 gap-0 md:gap-6 place-items-stretch p-0 md:p-5'>
+              <div className='w-full h-full grid grid-cols-2 sm:grid-cols-4 grid-rows-4 sm:grid-rows-2 gap-0 md:gap-6 place-items-stretch p-0 md:p-5'>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <MdOutlineDryCleaning className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <MdOutlineDryCleaning className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Dry Cleaning
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <GiDrippingStone className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <GiDrippingStone className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Wet Cleaning
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <MdOutlineLocalLaundryService className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <MdOutlineLocalLaundryService className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Laundry
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <MdOutlineIron className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <MdOutlineIron className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Iron Clothes
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <RiShirtLine className='text-[#76A6A6]  text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <RiShirtLine className='text-[#76A6A6]  text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Wash & Fold
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <GiSewingMachine className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <GiSewingMachine className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Cloth Sewing
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <AiOutlineEnvironment className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <AiOutlineEnvironment className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Free Pickup
                   </h2>
                 </div>
                 <div className='text-center p-5'>
-                  <div className='w-12 h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
-                    <MdOutlineDeliveryDining className='text-[#76A6A6] text-2xl md:text-4xl leading-none' />
+                  <div className='w-8 h-8 sm:w-12 sm:h-12 md:w-20 md:h-20 mx-auto bg-[#CEF2EF] rounded-full flex justify-center items-center'>
+                    <MdOutlineDeliveryDining className='text-[#76A6A6] text-lg sm:text-2xl md:text-4xl leading-none' />
                   </div>
-                  <h2 className='block text-base md:text-xl font-bold leading-none text-black mt-5 capitalize'>
+                  <h2 className='block text-xs sm:text-base md:text-xl font-bold leading-none text-black mt-2 sm:mt-5 capitalize'>
                     Free Delivery
                   </h2>
                 </div>
@@ -212,7 +212,7 @@ export default function Home() {
                     src='/imgs/local-laundry/man-iron-shirt.jpg'
                     width={300}
                     height={500}
-                    className='rounded-full h-[500px] md:h-[400px] lg:h-[500px]'
+                    className='rounded-full h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px]'
                   />
                 </div>
                 <div className='mt-32'>
@@ -220,7 +220,7 @@ export default function Home() {
                     src='/imgs/local-laundry/pile-laundry.jpg'
                     width={300}
                     height={500}
-                    className='rounded-full h-[500px] md:h-[400px] lg:h-[500px]'
+                    className='rounded-full h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px]'
                   />
                 </div>
               </div>
@@ -269,9 +269,9 @@ export default function Home() {
           </div>
           <div className="relative h-[40rem] w-full bg-[url('/imgs/local-laundry/clothes-hang.jpg')] bg-no-repeat bg-cover bg-center">
             <div className=''>
-              <div className='z-[10] text-white bg-white absolute top-[20%] left-[18%] w-[75%] p-8 shadow-2xl'>
-                <div className='w-[80%] mb-7'>
-                  <h1 className='text-black font-extrabold text-4xl lg:text-5xl leading-tight mb-6 capitalize'>
+              <div className='z-[10] text-white bg-white absolute top-[20%] left-0 right-0 mx-auto sm:left-[18%] w-[95%] sm:w-[75%] p-4 sm:p-8 shadow-2xl'>
+                <div className='w-full sm:w-[80%] mb-3 sm:mb-7'>
+                  <h1 className='text-black font-extrabold text-4xl lg:text-5xl leading-tight mb-3 sm:mb-6 capitalize'>
                     tincidunt tempus sapien Laundry Experience
                   </h1>
                   <p className='text-xs lg:text-xl lg:text-base font-medium text-black '>
@@ -286,47 +286,47 @@ export default function Home() {
                     libero, sollicitudin et leo at, ornare ultricies sapien
                   </p>
                 </div>
-                <div className='grid grid-cols-3 gap-6 items-end'>
+                <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 items-end'>
                   <div className='col-span-2'>
-                    <div className='grid grid-cols-2 gap-3 lg:gap-6 place-items-stretch'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-3 lg:gap-6 place-items-stretch'>
                       <div className='flex gap-3 justify-start items-start'>
-                        <AiOutlinePushpin className='text-xl lg:text-3xl text-[#D97941]' />
-                        <p className='text-sm lg:text-lg font-medium text-black'>
+                        <AiOutlinePushpin className='text-base sm:text-xl lg:text-3xl text-[#D97941]' />
+                        <p className='text-xs sm:text-sm lg:text-lg font-medium text-black'>
                           Lorem ipsum dolor
                         </p>
                       </div>
                       <div className='flex gap-3 justify-start items-start'>
-                        <AiOutlinePushpin className='text-xl lg:text-3xl text-[#D97941]' />
-                        <p className='text-sm lg:text-lg font-medium text-black'>
+                        <AiOutlinePushpin className='text-base sm:text-xl lg:text-3xl text-[#D97941]' />
+                        <p className='text-xs sm:text-sm lg:text-lg font-medium text-black'>
                           Nam bibendum tellus
                         </p>
                       </div>
                       <div className='flex gap-3 justify-start items-start'>
-                        <AiOutlinePushpin className='text-xl lg:text-3xl text-[#D97941]' />
-                        <p className='text-sm lg:text-lg font-medium text-black'>
+                        <AiOutlinePushpin className='text-base sm:text-xl lg:text-3xl text-[#D97941]' />
+                        <p className='text-xs sm:text-sm lg:text-lg font-medium text-black'>
                           Aenean eleifend quam
                         </p>
                       </div>
                       <div className='flex gap-3 justify-start items-start'>
-                        <AiOutlinePushpin className='text-xl lg:text-3xl text-[#D97941]' />
-                        <p className='text-sm lg:text-lg font-medium text-black'>
+                        <AiOutlinePushpin className='text-base sm:text-xl lg:text-3xl text-[#D97941]' />
+                        <p className='text-xs sm:text-sm lg:text-lg font-medium text-black'>
                           Nuis libero ultricies
                         </p>
                       </div>
                       <div className='flex gap-3 justify-start items-start'>
-                        <AiOutlinePushpin className='text-xl lg:text-3xl text-[#D97941]' />
-                        <p className='text-sm lg:text-lg font-medium text-black'>
+                        <AiOutlinePushpin className='text-base sm:text-xl lg:text-3xl text-[#D97941]' />
+                        <p className='text-xs sm:text-sm lg:text-lg font-medium text-black'>
                           Nunc aliquet sapien
                         </p>
                       </div>
                     </div>
-                    <div className='my-7'>
-                      <button className='text-base font-medium leading-none text-white px-6 py-3 bg-[#76A6A6]'>
+                    <div className='my-3 sm:my-7'>
+                      <button className='text-base font-medium leading-none text-white px-3 py-2 sm:px-6 sm:py-3 bg-[#76A6A6]'>
                         Our Services
                       </button>
                     </div>
                   </div>
-                  <div>
+                  <div className='order-first sm:order-none'>
                     <Image
                       src='/imgs/local-laundry/bath-towel.png'
                       width={300}
@@ -386,12 +386,12 @@ export default function Home() {
             </div>
           </div>
           <div className="relative w-full bg-[url('/imgs/local-laundry/delivery.jpg')] bg-no-repeat bg-cover bg-top">
-            <div className='w-full py-16 px-8 xl:px-16  2x:px-0 bg-transparent flex justify-start items-center'>
-              <div className='bg-white p-10 w-[60%] lg:w-[50%]'>
+            <div className='w-full py-16 px-4 sm:px-8 xl:px-16  2x:px-0 bg-transparent flex justify-start items-center'>
+              <div className='bg-white p-10 w-full sm:w-[60%] lg:w-[50%]'>
                 <h1 className='text-black font-extrabold text-4xl text-5xl leading-tight mb-6 capitalize'>
                   Free Pickup & Delivery
                 </h1>
-                <p className='text-xstext-sm font-medium text-black leading-loose'>
+                <p className='text-xs sm:text-sm font-medium text-black leading-loose'>
                   Curabitur augue quam, fringilla quis dictum sed, sodales non
                   eros. Nulla venenatis in odio non fringilla. Fusce facilisis
                   magna at odio ultricies, non porta quam scelerisque. Phasellus
@@ -481,12 +481,12 @@ export default function Home() {
       <footer>
         <div>
           <div className='w-full bg-[#CCF5F5]'>
-            <div className='max-w-[90rem] mx-auto p-20 mt-12 mb-24'>
+            <div className='max-w-[90rem] mx-auto px-8 py-20 sm:p-20 mt-12 mb-24'>
               <div className='w-full lg:w-[50%] mx-auto'>
                 <h3 className='text-black font-bold text-4xl leading-tight mb-8 capitalize text-center'>
                   Get a 2 months free trial for new members
                 </h3>
-                <div className='flex justify-center items-center gap-10'>
+                <div className='flex flex-col justify-center items-center gap-4 sm:gap-10'>
                   <input
                     type='email'
                     name='email'
@@ -516,7 +516,7 @@ export default function Home() {
                     magna at odio ultricies, non porta quam scelerisque.
                   </p>
                 </div>
-                <div className='md:col-span-2 grid grid-cols-3 place-items-stretch'>
+                <div className='md:col-span-2 grid grid-cols-1 sm:grid-cols-3 place-items-stretch gap-4'>
                   <div>
                     <h3 className='text-2xl font-semibold text-black leading-tight '>
                       Contact
@@ -561,10 +561,10 @@ export default function Home() {
           <div className='bg-[#76A6A6] w-full h-fit'>
             <div className='py-4 px-8 xl:px-16  max-w-[90rem] mx-auto'>
               <div className='w-full text-center'>
-                <p className='text-sm md:text-base font-semibold text-gray-100'>
+                <p className='text-xs sm:text-sm md:text-base font-semibold text-gray-100'>
                   &copy;2023 Easydry Services
-                  <span className='mx-4'> &middot; </span>All Rights Reserved
-                  <span className='mx-4'> &middot; </span>Designed by mmesoma
+                  <span className='mx-2 sm:mx-4'> &middot; </span>All Rights Reserved
+                  <span className='mx-2 sm:mx-4'> &middot; </span>Designed by mmesoma
                   saint
                 </p>
               </div>
