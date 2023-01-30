dd96c02fd671ceec2649346e103ccae3deb9ca20
Mmesoma Saint <mmesomasaint@gmail.com>
2023-01-30 05:49:10 +0100

    Fix: Import menu icon

--- pages/templates/local-laundry/career.js
+++ pages/templates/local-laundry/career.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { GiHamburgerMenu } from 'react-icons/gi'
 import { BsTelephone } from 'react-icons/bs'
 import { useCallback, useState } from 'react'
 
